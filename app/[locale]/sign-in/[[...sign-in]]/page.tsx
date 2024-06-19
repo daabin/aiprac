1274c54489bf1579a7198148f27adbacedbf87d4
wangbin <wangb@xiaopeng.com>
2024-06-19 16:08:29 +0800

    feat: clerk zh-cn

--- app/[locale]/sign-in/[[...sign-in]]/page.tsx
+++ app/[locale]/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,6 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return <div className="w-screen h-screen flex justify-center items-center">
-    <SignIn />
+    <SignIn routing="hash"/>
   </div>;
 }
